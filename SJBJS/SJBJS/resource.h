5effed3a8f837a64beb9c0fff57879959d20192f
SangdongJ <dlwjdtnrdjaa@naver.com>
2017-03-22 19:49:19 +0900

    Dialog build
    기본 다이알로그 구축 시작

--- SJBJS/SJBJS/resource.h
+++ SJBJS/SJBJS/resource.h
@@ -3,14 +3,20 @@
 // Resource.rc에서 사용되고 있습니다.
 //
 #define IDR_MENU                        102
+#define IDD_Tile                        103
+#define IDD_ta                          105
+#define IDD_MAP                         105
+#define ID_40001                        40001
+#define ID_40002                        40002
+#define ID_40003                        40003
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40004
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
