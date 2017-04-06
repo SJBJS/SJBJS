79e84118a85c3da39989bc2f34a02b42cf6692b5
정상동 <dlwjdtnrdjaa@naver.com>
2017-04-06 14:40:56 +0900

    tileSet Start
    기본 구축

--- SJBJS/DirectX11/PhysicsLogic.cpp
+++ SJBJS/DirectX11/PhysicsLogic.cpp
@@ -2,49 +2,82 @@
 
 PhysicsLogic::PhysicsLogic()
 {
+	TileWidth = 32;
+	TileMaxX = 10;
+	TileMaxY = 10;
+
 }
 
 
 PhysicsLogic::~PhysicsLogic()
 {
 }
-//bool PhysicsLogic::IsCollision()
-//{
-//}
-boolean PhysicsLogic::overlapPT(Player r1, Tile r2)
+bool PhysicsLogic::IsCollision()
 {
-	if (GetElement() &&
-		r1.leftLine < r2.leftLine + r2.width &&
-		r1.leftLine + r1.width > r2.leftLine &&
-		r1.leftLine < r2.leftLine + r2.height &&
-		r1.leftLine + r1.height > r2.leftLine)
+
+}
+boolean PhysicsLogic::overlapPT(Player r1, Tile r2)
+{   //첫 번째 조건절에서는 첫 번째 사각형의 왼쪽 모서리가 두 번째 사각형의 오른쪽 모서리보다 항상 
+	//왼쪽에 있어야 한다고 지정한다. 두 번째 조건문에서는 첫 번째 사각형의 오른쪽 모서리가 두 번째 사각형의 왼쪽 모서리보다 오른쪽에 있어야 한다
+	if (GetElement(r2) &&
+		r1.Prect.left < r2.Trect.left + r2.width &&
+		r1.Prect.left + r1.width > r2.Trect.left &&
+		r1.Prect.top  < r2.Trect.top + r2.height &&
+		r1.Prect.top + r1.height > r2.Trect.top)
 		return true;
 	else
 		false;
 }
 boolean PhysicsLogic::overlapPE(Player r1, Enermy r2)
 {
-	if (GetElement() &&
-		r1.leftLine < r2.leftLine + r2.width &&
-		r1.leftLine + r1.width > r2.leftLine &&
-		r1.leftLine < r2.leftLine + r2.height &&
-		r1.leftLine + r1.height > r2.leftLine)
+	if (r1.Prect.left  < r2.Erect.left + r2.width &&
+		r1.Prect.left + r1.width > r2.Erect.left &&
+		r1.Prect.top  < r2.Erect.top + r2.height &&
+		r1.Prect.top + r1.height > r2.Erect.top)
 		return true;
 	else
 		false;
 }
 boolean PhysicsLogic::overlapET(Enermy r1, Tile r2)
 {
-	if (GetElement() &&
-		r1.leftLine < r2.leftLine + r2.width &&
-		r1.leftLine + r1.width > r2.leftLine &&
-		r1.leftLine < r2.leftLine + r2.height &&
-		r1.leftLine + r1.height > r2.leftLine)
+	
+	if (GetElement(r2) &&
+		r1.Erect.left < r2.Trect.left + r2.width &&
+		r1.Erect.left + r1.width > r2.Trect.left &&
+		r1.Erect.top < r2.Trect.top + r2.height &&
+		r1.Erect.top + r1.height > r2.Trect.top)
 		return true;
 	else
 		false;
 }
-bool PhysicsLogic::GetElement()
+boolean PhysicsLogic::SetElement(Tile r1) {
+
+
+}
+bool PhysicsLogic::GetElement(Tile r1)
 {
-	return true;
+	if (r1.coll) {
+		return true;
+	}
+	else 
+		return false;
+}
+void PhysicsLogic::initTileSet() {
+	
+	Tile tile[10][10];
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			tile[i][j].Tndex = 0;
+			tile[i][j].coll = false;
+			tile[i][j].width = TileWidth;
+		}
+	}
+	// 임시 충돌 타일 인덱스 설정
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (j % 3 == 0) { //조건에 맞는 타일에 충돌 true 설정
+				tile[i][j].coll = true;
+			}
+		}
+	}
 }
--- SJBJS/DirectX11/PhysicsLogic.h
+++ SJBJS/DirectX11/PhysicsLogic.h
@@ -5,27 +5,39 @@
 #include "inputclass.h"
 #include "PlayerClass.h"
 
+
 typedef struct Tile{
-	int leftLine, topLine;
+	bool coll; //충돌체크
 	int width, height;
+	INT		Tndex;		// Tile Index
+	RECT	Trect;		// Tile Image Rect
+	XMFLOAT2	Tp;		// Tile Position
 	
 };
-typedef struct Player {
-	int leftLine, topLine;
+typedef struct Player {	
 	int width, height;
+	RECT	Prect;		// Player Image Rect
+	XMFLOAT2	Pp;		// Player Position
 };
 typedef struct Enermy {
-	int leftLine, topLine;
+	
 	int width, height;
+	RECT	Erect;		// Enermy Image Rect
+	XMFLOAT2	Ep;		// Enermy Position
 };
 class PhysicsLogic
 {
 public:
+	int TileWidth = 32;
+	int TileMaxX = 10;
+	int TileMaxY = 10;
+
 	PhysicsLogic();
 	~PhysicsLogic();
-
+	void initTileSet();
 	bool IsCollision(); //충돌 확인	
-	bool GetElement(); //해당 타일의 충돌가능 여부 속성
+	boolean SetElement(Tile r1); //타일 속성 설정
+	bool GetElement(Tile r1); //해당 타일의 충돌가능 여부 속성
 	boolean overlapPT(Player r1, Tile r2); // 플레이어와 타일 충돌확인
 	boolean overlapPE(Player r1, Enermy r2); // 플레이어와 적 충돌확인
 	boolean overlapET(Enermy r1, Tile r2); // 적과 타일 충돌확인
