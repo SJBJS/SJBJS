3dee46ef2f7555e99c86f5c9f3bc2d6e20b24251
정상동 <dlwjdtnrdjaa@naver.com>
2017-04-21 20:09:07 +0900

    background fix
    에러부분임시 주석처리

--- SJBJS/DirectX11/Background.cpp
+++ SJBJS/DirectX11/Background.cpp
@@ -22,24 +22,26 @@ bool Background::IsCollision()
 boolean Background::overlapPT(PlayerClass r1, Tile r2)
 {   //첫 번째 조건절에서는 첫 번째 사각형의 왼쪽 모서리가 두 번째 사각형의 오른쪽 모서리보다 항상 
 	//왼쪽에 있어야 한다고 지정한다. 두 번째 조건문에서는 첫 번째 사각형의 오른쪽 모서리가 두 번째 사각형의 왼쪽 모서리보다 오른쪽에 있어야 한다
-	if (GetElement(r2) &&
+	/*if (GetElement(r2) &&
 		r1.Prect.left < r2.Trect.left + r2.width &&
 		r1.Prect.left + r1.width > r2.Trect.left &&
 		r1.Prect.top  < r2.Trect.top + r2.height &&
 		r1.Prect.top + r1.height > r2.Trect.top)
 		return true;
 	else
-		false;
+		false;*/
+	return true;
 }
 boolean Background::overlapPE(PlayerClass r1, Enermy r2)
-{
+{/*
 	if (r1.Prect.left  < r2.Erect.left + r2.width &&
 		r1.Prect.left + r1.width > r2.Erect.left &&
 		r1.Prect.top  < r2.Erect.top + r2.height &&
 		r1.Prect.top + r1.height > r2.Erect.top)
 		return true;
 	else
-		return false;
+		return false;*/
+	return true;
 }
 boolean Background::overlapET(Enermy r1, Tile r2)
 {
