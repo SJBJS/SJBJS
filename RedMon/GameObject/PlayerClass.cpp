33a241f910bf61616a3575e764fea828623c8ffc
박정환 <park930919@gmail.com>
2017-05-17 17:53:46 +0900

    Update player

--- RedMon/GameObject/PlayerClass.cpp
+++ RedMon/GameObject/PlayerClass.cpp
@@ -11,12 +11,26 @@ void PlayerClass::Initialize()
 
 void PlayerClass::Update()
 {
+	float v = 0, h = 0;
+
 	if (Input->IsKeyDown(DIK_W))
-		position.y += -1.0f;
+		v -= 1.0f;
 	if (Input->IsKeyDown(DIK_S))
-		position.y += 1.0f;
+		v += 1.0f;
 	if (Input->IsKeyDown(DIK_A))
-		position.x += -1.0f;
+		h += -1.0f;
 	if (Input->IsKeyDown(DIK_D))
-		position.x += 1.0f;
+		h += 1.0f;
+
+	XMFLOAT3 dir(h, v, 0);
+	//안되는거
+	//float d = sqrt(dir.x*dir.x + dir.y*dir.y);
+	//XMFLOAT3 normal(dir.x/d,dir.y/d,0);
+
+	XMVECTOR vNormal = XMVector2Normalize(XMLoadFloat3(&dir));
+	XMFLOAT3 normal;
+	XMStoreFloat3(&normal, vNormal);
+	position += normal;
+	
+
 }
