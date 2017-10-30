ea01b885b25c69fdf60c9d21443770c5634186c7
Junghwan Park <park930919@gmail.com>
2017-10-30 14:03:38 +0900

    필요없는 슈팅관련 제거

--- RedMon/GameObject/PlayerClass.h
+++ RedMon/GameObject/PlayerClass.h
@@ -9,9 +9,6 @@
 class PlayerClass : public PawnClass
 {
 private:
-	int bulletNum;
-	size_t shotNum;
-	Bullet * myBullet;
 	float Windowx;
 	float speed; 
 	bool jumping;  //점프중인지 판단
@@ -34,31 +31,13 @@ public:
 		SetRotateFrozen(true);
 		SetGravityScale(1.0f);
 		
-		bulletNum = 10;
-		shotNum = 0;
-		/*myBullet = new Bullet[bulletNum];
-		for (int i = 0; i < bulletNum; ++i)
-		{
-			myBullet->Initialize();
-			myBullet[i].setWH(16, 16);
-			myBullet[i].setPower(10.0f);
-		}*/
-		
 	};
 
 	virtual void Update(float dt)
 	{
 		
-		static int i = 0;
-		i = 50;
-		SetTextureUV(0 + i, 0, imgOriginalSize.x, imgOriginalSize.y);
 		float v = 0, h = 0;
 			
-		
-		/*if (Input->IsKeyPressed(DIK_Q))
-			Rotate(5 * dt);
-		if (Input->IsKeyPressed(DIK_E))
-			Rotate(-5 * dt);*/
 		if (!jumping) {
 			Move(0, -2);
 			if (position.y <= -120) {
@@ -79,24 +58,6 @@ public:
 			vector<ActorClass*>playerHP1 = ObjectManager::Instance()->FindObjectsWithTag("HP");
 			playerHP1[i]->SetPosition(1200, 0);
 		}
-		
-		/*
-		vector<ActorClass*>target = ObjectManager::Instance()->FindObjectsWithTag("Box");
-		for (int i = 0; i < 3; i++) {
-			XMFLOAT3 targetposition = target[i]->GetPosition();
-			XMFLOAT3 dir1 = targetposition - position;
-			if (dir1.x < 20) {
-				if (dir1.y < 20) {
-					HP--;
-					target[i]->SetPosition(500, -200);
-					if (HP <= 0)
-					{
-						OnDie();
-					}
-				}
-			};
-		}
-		*/
 
 		XMFLOAT3 dir(h, v, 0);
 		XMVECTOR vNormal = XMVector2Normalize(XMLoadFloat3(&dir));
@@ -133,15 +94,6 @@ public:
 	{
 	}
 	
-	void Fire()
-	{
-		if (myBullet[shotNum].IsFire())
-			return;
-		myBullet[shotNum].Fire(true);
-		
-		myBullet[shotNum].Spwan(this->position,this->rotate,XMFLOAT3(0, 60, 0));
-		shotNum = (shotNum + 1) % bulletNum;
-	}
 
 	void OnDie()
 	{
